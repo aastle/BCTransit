33f14fe26402e58a65314d36ded39fc90f8d09ce
terrapinbear <aastle@gmail.com>
2014-07-02 13:04:09 -0400

    Added html tags

--- src/me/aastle/Main.java
+++ src/me/aastle/Main.java
@@ -9,13 +9,22 @@ public class Main {
     final static String url = "http://www.ridebctransit.com/transit/35-outbound-weekday";
     public static void main(String[] args) {
         try {
+            System.out.println("<!DOCTYPE html>");
+            System.out.println("<html lang=\"en\">");
+            System.out.println("<head>");
+            System.out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">");
+            System.out.println("</head>");
+            System.out.println("<body>");
             Document document = Jsoup.connect(url).get();
-            for(Element element : document.select("table tr")){
-                System.out.println(element.text());
+            for(Element element : document.select("table")){
+                System.out.println(element.toString() + element.text());
             }
             for(Element element : document.getElementById("center").select("p")){
-              System.out.println(element.text());
+              System.out.println(element.toString() + element.text());
             }
+            System.out.println("</body>");
+            System.out.println("</html>");
+
         }catch (IOException io){
             System.out.println(io.fillInStackTrace());
         }
